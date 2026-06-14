dcc2682c14ba745e84138de41a4410d1512e2a25
dominguez-j <chondominguez55@gmail.com>
2026-06-14 15:27:12 -0300

    Agrego el protorouter y setteo puerto en la topo

--- topo.py
+++ topo.py
@@ -32,14 +32,12 @@ class NATTopo(Topo):
     def build(self):
         s1 = self.addSwitch('s1')
 
-        h1 = self.addHost('h1', ip='200.0.0.1/24',
-                          mac='00:00:00:00:00:01', defaultRoute='via 200.0.0.254')
+        h1 = self.addHost('h1', ip='200.0.0.1/24', mac='00:00:00:00:00:01', defaultRoute='via 200.0.0.254')
 
-        h2 = self.addHost('h2', ip='192.168.1.2/24', mac='00:00:00:00:00:02',
-                          defaultRoute='via 192.168.1.254')
+        h2 = self.addHost('h2', ip='192.168.1.2/24', mac='00:00:00:00:00:02', defaultRoute='via 192.168.1.254')
 
-        self.addLink(h1, s1)
-        self.addLink(h2, s1)
+        self.addLink(h1, s1, port2=1)
+        self.addLink(h2, s1, port2=2)
 
 
 def run():
